ca357f10db984925a2f9f27f93a7b0c96b509f23
Puf1k-0u0 <you@example.com>
2024-09-17 16:23:16 +0300

    Add regstration, authorization and user profile

--- authorization/models.py
+++ authorization/models.py
@@ -1,1 +1,13 @@
-from authorization import db
+from flask_login import UserMixin
+
+from authorization import db, login_manager
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), nullable=False, unique=True)
+    password = db.Column(db.Text(), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
